ebc03f0e71319864d9371a5573b1fa224d949d9c
alexdabson07@gmail.com <alexdabson07@gmail.com>
2024-10-10 21:58:43 +0000

    Add project files.

---
+++ Code Signal Interview Prep/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Code_Signal_Interview_Prep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
